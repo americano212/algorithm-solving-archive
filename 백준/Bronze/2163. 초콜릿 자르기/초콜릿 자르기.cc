a266159dc476c2a709530a5eba79a031d9182024
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-02-07 01:08:48 -0800

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,15 @@
+#define ll long long
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    ll X,Y,x,y,N,M,a,b;
+    cin >> N >> M;
+    cout << N*M - 1;
+
+    
+    return 0;
+}
