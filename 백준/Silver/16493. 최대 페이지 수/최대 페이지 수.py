4ecd7f62b813e4eefc66d9f1fa96a3749ae6d05e
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-02-14 21:09:16 +0900

    [Silver II] Title: 최대 페이지 수, Time: 172 ms, Memory: 116208 KB -BaekjoonHub

---
+++ 백준/Silver/16493. 최대 페이지 수/최대 페이지 수.py
@@ -0,0 +1,24 @@
+import sys
+import copy
+from collections import deque
+
+input = sys.stdin.readline
+ans = 0
+cnt = 0
+
+k, n = map(int, input().split())
+
+thing = [[0, 0]]
+
+for i in range(n):
+    thing.append(list(map(int, input().split())))
+dp = [[0] * (k + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, k + 1):
+        w, v = thing[i]
+        if j < w:
+            dp[i][j] = dp[i - 1][j]
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
+print(dp[n][k])
