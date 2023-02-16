82e006e373277d165efba48f700bd1b98c39b792
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-02-17 02:00:07 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <functional>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <cstring>
+#include <cassert>
+using ll = long long;
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	
+	ll n, m;
+	cin >> n >> m;
+	cout << abs(n - m) << endl;
+	return 0;
+}
