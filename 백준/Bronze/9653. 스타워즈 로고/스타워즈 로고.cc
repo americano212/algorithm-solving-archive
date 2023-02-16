74f101530327298171fd87a150c6efcb193f066f
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-02-17 02:14:52 +0900

    [Bronze V] Title: 스타워즈 로고, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9653. 스타워즈 로고/스타워즈 로고.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <functional>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <cstring>
+#include <cassert>
+using ll = long long;
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	
+	cout << "    8888888888  888    88888" << endl;
+	cout << "   88     88   88 88   88  88" << endl;
+	cout << "    8888  88  88   88  88888" << endl;
+	cout << "       88 88 888888888 88   88" << endl;
+	cout << "88888888  88 88     88 88    888888" << endl;
+	cout << endl;
+	cout << "88  88  88   888    88888    888888" << endl;
+	cout << "88  88  88  88 88   88  88  88" << endl;
+	cout << "88 8888 88 88   88  88888    8888" << endl;
+	cout << " 888  888 888888888 88  88      88" << endl;
+	cout << "  88  88  88     88 88   88888888" << endl;
+
+
+
+
+
+	return 0;
+}
