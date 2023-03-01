84595c2646a6b39a10cbe100234cdfd8ce32439c
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-03-01 21:49:07 +0900

    [Silver II] Title: N번째 큰 수, Time: 424 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <functional>
+#include <climits>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <cstring>
+#include <cassert>
+using ll = long long;
+using namespace std;
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0), cout.tie(0);
+	
+	int n;
+	int temp;
+	cin >> n;
+	priority_queue<int, vector<int>, greater<int>> pq;
+	for (int i = 0; i < n * n; i++) {
+		cin >> temp;
+		pq.push(temp);
+		if (pq.size() > n) {
+			pq.pop();
+		}
+	}
+	cout << pq.top() << endl;
+	return 0;
+}
