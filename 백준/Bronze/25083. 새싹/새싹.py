4530fceb8cdc274bc553a97ce9fe655da9ba085d
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-03-03 22:57:20 +0900

    [Bronze V] Title: 새싹, Time: 160 ms, Memory: 116056 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.py
@@ -0,0 +1,19 @@
+import sys
+import copy
+import heapq
+from collections import deque
+from math import inf
+
+# sys.setrecursionlimit(100000)
+
+input = sys.stdin.readline
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+print("""         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |""")
