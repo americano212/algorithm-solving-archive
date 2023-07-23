8bf16301962f03a1a4b24e25352a0661c180750b
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-07-23 02:48:31 +0000

    S2_15663_solve

--- boj/py/15663.py
+++ boj/py/15663.py
@@ -1,4 +1,30 @@
-from itertools import permutations
 n,m = map(int,input().split())
 L = list(map(int,input().split()))
+L.sort()
+visit = []
 
+def dfs():
+    if(len(visit) == m):
+        result.append(' '.join(map(str, visit)))
+        return
+    for item in L:
+        if(visit == []):
+            visit.append(item)
+            dfs()
+            visit.pop()
+        else:
+            if(visit.count(item) != L.count(item)):
+                visit.append(item)
+                dfs()
+                visit.pop()
+
+result = []
+dfs()
+
+result = list(set(result))
+result_int = []
+for item in result:
+    result_int.append(list(map(int, item.split())))
+result_int.sort()
+for prt in result_int:
+    print(*prt)
