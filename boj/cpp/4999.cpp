5fc2864216790bf0b1e362ee71541d500c62daae
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-08-06 07:14:13 +0000

    B2_1076_solve

---
+++ boj/cpp/4999.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define endl '\n';
+using namespace std;
+typedef long long int ll;
+typedef vector<vector<ll>> matrix;
+
+const ll mod = 1000000007LL;
+
+ll a, b, c, i, j, k, n, m, t;
+ll test;
+string s1, s2;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> s1;
+    cin >> s2;
+    if(s1.length() >= s2.length()){
+        cout << "go" << endl;
+    }else{
+        cout << "no" << endl;
+    }
+}
--- solved.py
+++ solved.py
@@ -1,4 +1,8 @@
-for i in range(20):
-    problem = 515 + i
+score_save = []
+for i in range(1, 21):
+    problem = 522 + i
     score = 175 * (1 - 0.995**problem)
+    score_before = 175 * (1 - 0.995**(problem - 1))
+    if (int(score) != int(score_before)):
+        print('*'*50)
     print("{}문제 후".format(i), score)
