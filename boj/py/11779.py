bb085285ccda9bc7f4d98fcc0ca283fded9a5eee
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-08-07 03:50:56 +0000

    G3_11779_solve

---
+++ boj/py/11779.py
@@ -0,0 +1,46 @@
+from collections import deque
+import sys
+import heapq
+INF = sys.maxsize
+
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+v = [[] for _ in range(n+1)]
+
+
+for _ in range(m):
+    start, end, cost = map(int, input().split())
+    v[start].append([end, cost])
+start_node, end_node = map(int, input().split())
+
+distance = [INF] * (n+1)
+way = [0 for _ in range(n+1)]
+
+queue = [[start_node, 0]]
+
+way[start_node] = start_node
+
+while (queue):
+    now_node, now_cost = heapq.heappop(queue)
+    if (distance[now_node] < now_cost):
+        continue
+    for new_node, new_cost in v[now_node]:
+        if (distance[new_node] > new_cost + now_cost):
+            distance[new_node] = new_cost + now_cost
+            way[new_node] = now_node
+            heapq.heappush(queue, [new_node, new_cost + now_cost])
+print(distance[end_node])
+way_result = deque()
+way_result.append(end_node)
+final_way = []
+while (way_result):
+    node = way_result.popleft()
+    final_way.append(node)
+    if (node == start_node):
+        break
+    way_result.append(way[node])
+print(len(final_way))
+print(*final_way[::-1])
