7bf6d7cabf86f4bd41ff37e378f34442e31d1d30
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-08-23 11:40:07 +0000

    G4_1956_solve

--- boj/py/1956.py
+++ boj/py/1956.py
@@ -1,30 +1,22 @@
-from collections import deque
 INF = float('inf')
-v, e = map(int, input().split())
+n, m = map(int, input().split())
+graph = [[INF]*(n+1) for _ in range(n+1)]
 
-graph = [[] for _ in range(v+1)]
+for _ in range(m):
+    src, dst, cost = map(int, input().split())
+    graph[src][dst] = cost
 
-for _ in range(e):
-    a, b, c = map(int, input().split())
-    graph[a].append([b, c])
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            graph[i][j] = min(graph[i][j], graph[i][k]+graph[k][j])
 
-queue = deque()
-
+# for items in graph:
+#     print(*items)
 result = INF
-for start_node in range(1, v+1):
-    queue.append([start_node, 0])
-    distance = [INF] * (v+1)
-    distance[start_node] = 0
-
-    while (queue):
-        now_node, now_cost = queue.popleft()
-        if (now_cost > distance[now_node]):
-            continue
-        for next_node, next_cost in graph[now_node]:
-            if (next_node == start_node):
-                result = min(result, now_cost+next_cost)
-                continue
-            if (distance[next_node] > now_cost+next_cost):
-                distance[next_node] = now_cost+next_cost
-                queue.append([next_node, next_cost])
-print(result)
+for i in range(1, n):
+    result = min(result, graph[i][i])
+if (result == INF):
+    print(-1)
+else:
+    print(result)
