bc89a8dc9148fa99af1879b2277f52f8b6370eea
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-08-26 04:48:04 +0000

    G2_1918_solve

--- boj/py/1918.py
+++ boj/py/1918.py
@@ -1,56 +1,27 @@
 s = list(input())
 
 stack = []
-while (s):
-    now = s.pop()
-    if (now == '('):
-        result = ''
-        while (True):
-            before = stack.pop()
-            if (before == ')'):
-                break
-            result += before
-        keep = ''
-        result_L = []
-        for i in range(len(result)):
-            now_r = result[i]
-            if (now_r == '+' or now_r == '-'):
-                keep = now_r
-                continue
-            if (keep != ''):
-                result_L.append(now_r + keep)
-            else:
-                result_L.append(now_r)
+result = ''
 
-        stack.append(''.join(result_L))
-
-        if (len(stack) != 0):
-            if (stack[-1] == '*' or stack[-1] == '/'):
-                before = stack.pop()
-                before_before = stack.pop()
-                result = now + before_before + before
-                stack.append(result)
-    elif (now == ')'):
-        stack.append(now)
-    elif (len(stack) != 0):
-        if (stack[-1] == '*' or stack[-1] == '/'):
-            before = stack.pop()
-            before_before = stack.pop()
-            result = now + before_before + before
-            stack.append(result)
-        else:
+for i in range(len(s)):
+    now = s[i]
+    if (now.isalpha()):
+        result += now
+    else:
+        if (now == '('):
             stack.append(now)
-    else:
-        stack.append(now)
-    # print(stack)
-final = []
-for i in range(len(stack)):
-    final_r = stack[i]
-    if (final_r == '-' or final_r == '+'):
-        before = final.pop()
-        final.append(before+final_r)
-    else:
-        final.append(final_r)
+        elif (now == '*' or now == '/'):
+            while (len(stack) != 0 and (stack[-1] == '*' or stack[-1] == '/')):
+                result += stack.pop()
+            stack.append(now)
+        elif (now == '+' or now == '-'):
+            while (len(stack) != 0 and stack[-1] != '('):
+                result += stack.pop()
+            stack.append(now)
+        elif (now == ')'):
+            while (len(stack) != 0 and stack[-1] != '('):
+                result += stack.pop()
+            stack.pop()
 
-final.reverse()
-print(''.join(final))
+stack.reverse()
+print(result + ''.join(stack))
