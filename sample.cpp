e0f81f8ad2e0e6e2def072ec1ecb6accac67dc07
Dongjun Shin <53344349+wq0212@users.noreply.github.com>
2023-08-27 10:21:00 +0000

    cpp sample update

--- sample.cpp
+++ sample.cpp
@@ -1,9 +1,10 @@
 #include <bits/stdc++.h>
-
 #define endl '\n';
 #define INF 2147483647;
-using namespace std;
-typedef long long int ll;
+#define ll long long
+#define pll pair<ll, ll>
+#define lcm(a, b) a *b / gcd(a, b)
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
 typedef vector<vector<ll>> matrix;
 
 const ll mod = 1000000007LL;
@@ -11,8 +12,20 @@ const ll mod = 1000000007LL;
 ll a, b, c, i, j, k, n, m, t;
 ll test;
 
+using namespace std;
+
+void solve()
+{
+}
+
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)
+        solve();
+
+    return 0;
 }
