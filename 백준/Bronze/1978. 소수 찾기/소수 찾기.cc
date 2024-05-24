fd82e8b4c0d3345b838bcafc38ba07e5de34720a
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-05-24 23:57:02 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2147483647
+#define ll long long
+#define pll pair<ll, ll>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a *b / gcd(a, b)
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1000000007LL;
+
+using namespace std;
+
+void solve() {
+    vector<ll> L(100001);
+
+    bool prime[1001];
+
+    FOR(i, 0, 1001) {
+        prime[i] = true;
+    }
+    prime[1] = false;
+    FOR(i, 2, 1001) {
+        if (prime[i] == true) {
+            int cnt = 2;
+            while (1) {
+                if (i * cnt > 1000) break;
+                prime[i * cnt] = false;
+                cnt++;
+            }
+        }
+    }
+    int result = 0;
+    int N, target;
+    cin >> N;
+    FOR(i, 0, N) {
+        cin >> target;
+        if (prime[target]) result++;
+    }
+    cout << result << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--) solve();
+
+    return 0;
+}
