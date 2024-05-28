04385dcb394076841f8fdf78d39def619904169c
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-05-28 17:53:22 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2147483647
+#define ll long long
+#define pll pair<ll, ll>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a *b / gcd(a, b)
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1000000007LL;
+
+using namespace std;
+
+bool cmp(pair <int, int> a, pair <int, int> b) {
+    if (a.first == b.first) return a.second < b.second;
+    return a.first < b.first;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> v(n, 0);
+    FOR(i, 0, n) {
+        cin >> v[i];
+    }
+    vector<int> cache(n, 1);
+    FOR(i, 0, n) {
+        int max_cache = 0;
+        FOR(j, 0, i) {
+            if (v[i] > v[j]) {
+                max_cache = max(max_cache, cache[j]);
+            }
+        }
+        cache[i] = max_cache + 1;
+    }
+
+    cout << *max_element(cache.begin(), cache.end()) << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--) solve();
+
+    return 0;
+}
