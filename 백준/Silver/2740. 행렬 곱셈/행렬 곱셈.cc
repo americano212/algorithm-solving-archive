a2394300b9a1a9263f25633ee03a7c43c1dfec7b
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-05-29 20:32:15 +0900

    [Silver V] Title: 행렬 곱셈, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2147483647
+#define ll long long
+#define pll pair<ll, ll>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a *b / gcd(a, b)
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1000000007LL;
+
+using namespace std;
+
+bool cmp(pair <int, int> a, pair <int, int> b) {
+    if (a.first == b.first) return a.second < b.second;
+    return a.first < b.first;
+}
+
+
+
+void solve() {
+    int N, M, K;
+
+    cin >> N >> M;
+
+    matrix A(N, vector<ll>(M, 0));
+
+    FOR(i, 0, N) {
+        FOR(j, 0, M) {
+            cin >> A[i][j];
+        }
+    }
+
+    cin >> M >> K;
+    matrix B(M, vector<ll>(K, 0));
+
+    FOR(i, 0, M) {
+        FOR(j, 0, K) {
+            cin >> B[i][j];
+        }
+    }
+
+    matrix answer(N, vector<ll>(K, 0));
+
+    FOR(i, 0, N) {
+        FOR(j, 0, K) {
+            int tmp = 0;
+            FOR(k, 0, M) {
+                tmp += A[i][k] * B[k][j];
+            }
+            cout << tmp << ' ';
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--) solve();
+
+    return 0;
+}
