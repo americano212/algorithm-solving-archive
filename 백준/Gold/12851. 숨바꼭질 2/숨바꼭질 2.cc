8d0dd31750172ef13bef6252ab3b4199fa55aab4
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-06-03 20:04:36 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 4 ms, Memory: 3336 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2147483647
+#define ll long long
+#define pll pair<ll, ll>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a *b / gcd(a, b)
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1000000007LL;
+
+using namespace std;
+
+bool cmp(pair <int, int> a, pair <int, int> b) {
+    if (a.first == b.first) return a.second < b.second;
+    return a.first < b.first;
+}
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> distance(100001, -1);
+    vector<int> cnt(100001, 0);
+    queue<pll> qu;
+
+    qu.push(make_pair(n, 0));
+    distance[n] = 0;
+    cnt[n] = 1;
+
+    int node, time;
+    pll tmp;
+    while (!qu.empty()) {
+        tmp = qu.front();
+        qu.pop();
+        node = tmp.first;
+        time = tmp.second;
+
+        //if (node == k) {
+        //    cout << time << endl;
+        //    break;
+        //}
+
+        int choose[3] = { node - 1 ,node + 1, 2 * node };
+        FOR(i, 0, 3) {
+            int next_node = choose[i];
+            if (0 <= next_node && next_node <= 100000) {
+                if (distance[next_node] == -1) {
+                    distance[next_node] = distance[node] + 1;
+                    cnt[next_node] = cnt[node];
+                    qu.push(make_pair(next_node, time + 1));
+                }
+                else {
+                    if (distance[next_node] == distance[node] + 1) {
+                        cnt[next_node] += cnt[node];
+                    }
+                }
+            }
+        }
+    }
+
+    cout << distance[k] << endl;
+    cout << cnt[k] << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--) solve();
+
+    return 0;
+}
