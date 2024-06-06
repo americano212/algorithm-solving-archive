9a521a0f2328386f6ef8b06c68a9e50133e6a111
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-06-06 20:33:42 +0900

    [Silver II] Title: N과 M (12), Time: 8 ms, Memory: 3076 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2147483647
+#define ll long long
+#define pll pair<ll, ll>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a *b / gcd(a, b)
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1000000007LL;
+
+using namespace std;
+
+bool cmp(pair <int, int> a, pair <int, int> b) {
+    if (a.first == b.first) return a.second < b.second;
+    return a.first < b.first;
+}
+
+int n, m;
+
+vector<vector<int>> ans;
+
+void recur(vector<int>& v, vector<int>& target, vector<bool>& visit) {
+    vector<int> tmp;
+    if (v.size() == m) {
+        FOR(i, 0, m) tmp.push_back(v[i]);
+        ans.push_back(tmp);
+        return;
+    }
+
+    FOR(i, 0, n) {
+        if (visit[i]) continue;
+        
+        if (!v.empty()) {
+            if (v.back() <= target[i]) {
+                v.push_back(target[i]);
+                recur(v, target, visit);
+                v.pop_back();
+            }
+        }
+        else {
+            v.push_back(target[i]);
+            recur(v, target, visit);
+            v.pop_back();
+        }
+    }
+}
+
+void solve() {
+    cin >> n >> m;
+
+    vector<int> v;
+    vector<int> target(n);
+    vector<bool> visit(n + 1, false);
+    FOR(i, 0, n) {
+        cin >> target[i];
+    }
+    sort(target.begin(), target.end());
+    recur(v, target, visit);
+    sort(ans.begin(), ans.end());
+    ans.erase(unique(ans.begin(), ans.end()), ans.end());
+    FOR(i, 0, ans.size()) {
+        FOR(j, 0, ans[i].size()) {
+            cout << ans[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--) solve();
+
+    return 0;
+}
