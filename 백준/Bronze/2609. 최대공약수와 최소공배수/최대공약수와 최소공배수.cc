11a8d2fd4e6aa53316967261b2e6da296e456bb9
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-06-11 22:58:23 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2147483647
+#define ll long long
+#define pll pair<ll, ll>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a *b / gcd(a, b)
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1000000007LL;
+
+using namespace std;
+
+bool cmp(pair <int, int> a, pair <int, int> b) {
+    if (a.first == b.first) return a.second < b.second;
+    return a.first < b.first;
+}
+
+
+void solve() {
+    ll a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << endl;
+    cout << lcm(a, b) << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--) solve();
+
+    return 0;
+}
