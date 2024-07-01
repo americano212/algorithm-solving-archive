b8f0457420d4ba7b599ad8aa43576ff804084ec4
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-07-01 19:27:27 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2147483647
+#define ll long long
+#define pll pair<ll, ll>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a *b / gcd(a, b)
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1000000007LL;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+
+using namespace std;
+
+bool cmp(pair <int, int> a, pair <int, int> b) {
+    if (a.first == b.first) return a.second < b.second;
+    return a.first < b.first;
+}
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<ll> v(n);
+    FOR(i, 0, n) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    int ans = v[0];
+
+    FOR(i, 1, n) {
+        v[i] += v[i - 1];
+        ans += v[i];
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    //cin >> tc;
+    while (tc--) solve();
+
+    return 0;
+}
