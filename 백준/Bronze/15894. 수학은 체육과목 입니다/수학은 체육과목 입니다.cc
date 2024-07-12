fe447a34881b823f3c36754b15909cc83050c0dd
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-07-12 15:33:23 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647
+#define ll long long
+#define pll pair<ll, ll>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'007LL;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+
+using namespace std;
+
+
+void solve() {
+    ll n;
+    cin >> n;
+    cout << 4 * n << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
