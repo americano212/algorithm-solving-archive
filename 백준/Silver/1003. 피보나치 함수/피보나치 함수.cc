e686cfe697216c067b134c1368fe014d9de97d03
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-07-13 13:28:37 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647
+#define ll long long
+#define pll pair<ll, ll>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'007LL;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+
+using namespace std;
+
+
+void solve() {
+    int t;
+    cin >> t;
+    vector<pll> dp(41, { 0,0 });
+    dp[0] = { 1, 0 };
+    dp[1] = { 0, 1 };
+    FOR(i, 2, 41) {
+        dp[i].first = dp[i - 1].first + dp[i - 2].first;
+        dp[i].second = dp[i - 1].second + dp[i - 2].second;
+    }
+    FOR(_, 0, t) {
+        int n;
+        cin >> n;
+        cout << dp[n].first << ' ' << dp[n].second << endl;
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
