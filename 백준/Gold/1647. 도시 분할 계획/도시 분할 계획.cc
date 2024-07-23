750fbd25168aae135b81d378d6cdff3940b37cc6
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-07-23 20:49:30 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 544 ms, Memory: 39408 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.cc
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647
+#define ll long long
+#define ld long double
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'007LL;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct COORDINATE { ll x; ll y; ll z; };
+
+vector<int> parent(100001);
+
+int Find(int x) {
+    if (parent[x] == x)return x;
+    return parent[x] = Find(parent[x]);
+}
+
+void Union(int x, int y) {
+    int a = Find(x);
+    int b = Find(y);
+    if (a < b) parent[a] = b;
+    else parent[b] = a;
+}
+
+bool Same(int x, int y) {
+    int a = Find(x);
+    int b = Find(y);
+    return a == b;
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    priority_queue<pllp, vector<pllp>, greater<pllp>> pq;
+    int a, b, c;
+    FOR(i, 0, m) {
+        cin >> a >> b >> c;
+        pq.push({ c,{a,b} });
+    }
+
+    FOR(i, 0, 100001) {
+        parent[i] = i;
+    }
+
+    int maximum_road = -1;
+
+    ll ans = 0;
+
+    while (!pq.empty()) {
+        int cost = pq.top().first;
+        int x = pq.top().second.first;
+        int y = pq.top().second.second;
+        pq.pop();
+
+        if (!Same(x, y)) {
+            ans += cost;
+            maximum_road = max(maximum_road, cost);
+            Union(x, y);
+        }
+    }
+    cout << ans - maximum_road << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
