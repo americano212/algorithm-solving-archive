27a1e4de7b873301edb97481026da2da50284af3
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-07-25 20:22:51 +0900

    [Gold III] Title: 별자리 만들기, Time: 0 ms, Memory: 2548 KB -BaekjoonHub

---
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.cc
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647
+#define ll long long
+#define ld long double
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'007LL;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct COORDINATE { ll x; ll y; ll z; };
+
+vector<int> parent(100);
+
+int Find(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = Find(parent[x]);
+}
+
+void Union(int x, int y) {
+    int a = Find(x);
+    int b = Find(y);
+    if (a < b) parent[a] = b;
+    else parent[b] = a;
+}
+
+bool Same(int x, int y) {
+    int a = Find(x);
+    int b = Find(y);
+    return a == b;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<pff> v(n);
+
+    FOR(i, 0, n) {
+        cin >> v[i].first >> v[i].second;
+    }
+
+    priority_queue<pair<double, pll>, vector<pair<double, pll>>, greater<pair<double, pll>> > pq;
+
+    FOR(i, 0, n) {
+        FOR(j, i + 1, n) {
+            ll x1 = v[i].first, x2 = v[j].first;
+            ll y1 = v[i].second, y2 = v[j].second;
+            pq.push({ sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)), {i, j} });
+        }
+    }
+
+    FOR(i, 0, 100) {
+        parent[i] = i;
+    }
+
+
+    float ans = 0;
+    while (!pq.empty()) {
+        float cost = pq.top().first;
+        int a = pq.top().second.first;
+        int b = pq.top().second.second;
+        pq.pop();
+
+        if (!Same(a, b)) {
+            ans += cost;
+            Union(a, b);
+        }
+    }
+    cout.precision(4);
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
