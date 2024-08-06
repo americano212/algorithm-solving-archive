bfe05e676ffc0dced0260175379a1e2fd330de8d
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-08-06 15:02:51 +0900

    [Bronze II] Title: 부분 문자열, Time: 24 ms, Memory: 5124 KB -BaekjoonHub

---
+++ 백준/Bronze/16916. 부분 문자열/부분 문자열.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647ll
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'007LL;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct COORDINATE { ll x; ll y; ll z; };
+
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+
+
+void solve() {
+    string s, p;
+    cin >> s >> p;
+    if (strstr(s.c_str(), p.c_str()) == NULL) {
+        cout << 0 << endl;
+    }
+    else {
+        cout << 1 << endl;
+    }
+}
+
+
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
