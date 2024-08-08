9a01be8990c0fb718fc3699a35334c0e12ba7b91
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-08-08 20:22:58 +0900

    [Gold V] Title: 동전 2, Time: 4 ms, Memory: 2180 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.cc
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647ll
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'007LL;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct COORDINATE { ll x; ll y; ll z; };
+
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vector<int> v(n);
+    FOR(i, 0, n) {
+        cin >> v[i];
+    }
+
+    vector<int> dp(k + 1, INF);
+    dp[0] = 0;
+
+    FOR(i, 0, n) {
+        int coin = v[i];
+        FOR(j, 1, k + 1) {
+            if (j - coin >= 0) {
+                if (dp[j - coin] == INF) continue;
+                dp[j] = min(dp[j], dp[j - coin] + 1);
+            }
+        }
+        //FOR(j, 1, k + 1) {
+        //    cout << dp[j] << ' ';
+        //}
+        //cout << endl;
+    }
+
+    if (dp[k] == INF) {
+        cout << -1 << endl;
+    }
+    else {
+        cout << dp[k] << endl;
+    }
+}
+
+
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
