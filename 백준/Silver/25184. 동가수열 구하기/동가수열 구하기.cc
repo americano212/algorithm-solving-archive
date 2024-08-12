2c6f1fa9f341881b9158060c41735727d07de4ac
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-08-13 00:23:27 +0900

    [Silver IV] Title: 동가수열 구하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/25184. 동가수열 구하기/동가수열 구하기.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647ll
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'000LL;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct COORDINATE { ll x; ll y; ll z; };
+
+struct ITEM { int cnt; int rx; int ry; int bx; int by; };
+
+void solve() {
+    int n;
+    cin >> n;
+    int temp = n;
+    if (n % 2 == 0) {
+        int mid = n / 2;
+        FOR(i, 0, n / 2) {
+            cout << mid << ' ' << temp << ' ';
+            mid -= 1;
+            temp -= 1;
+        }
+        cout << endl;
+    }
+    else {
+        int mid = n / 2 + 1;
+        FOR(i, 0, n / 2) {
+            cout << mid << ' ' << temp << ' ';
+            mid -= 1;
+            temp -= 1;
+        }
+        cout << 1 << endl;
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
