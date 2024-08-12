5ebaf2d82a79c926d30b9c75ad43b16f8a9471f1
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-08-13 00:10:59 +0900

    [Silver V] Title: 알파빌과 베타빌, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/29615. 알파빌과 베타빌/알파빌과 베타빌.cc
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647ll
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'000LL;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct COORDINATE { ll x; ll y; ll z; };
+
+struct ITEM { int cnt; int rx; int ry; int bx; int by; };
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<int> v(n);
+
+    FOR(i, 0, n) {
+        cin >> v[i];
+    }
+    int frd;
+    int ans = 0;
+    FOR(j, 0, m) {
+        cin >> frd;
+        FOR(j, m, n) {
+            if (frd == v[j]) {
+                ans++;
+                break;
+            }
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
