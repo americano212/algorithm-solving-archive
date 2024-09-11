63c500121d74e9ad2c8840505352c6585b838811
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-09-11 20:44:26 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'000LL;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,1,0,-1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { int x; int y; int dir; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+
+
+void solve() {
+    vector<bool> isSKWin(1001, true);
+    isSKWin[1] = false;
+    isSKWin[2] = true;
+    isSKWin[3] = false;
+    isSKWin[4] = true;
+
+    FOR(i, 5, 1001) {
+        bool win = false;
+        if (isSKWin[i - 1] == false || isSKWin[i - 3] == false || isSKWin[i - 4] == false) {
+            win = true;
+        }
+        isSKWin[i] = win;
+    }
+    int n;
+    cin >> n;
+    if (isSKWin[n]) {
+        cout << "SK" << endl;
+    }
+    else {
+        cout << "CY" << endl;
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
