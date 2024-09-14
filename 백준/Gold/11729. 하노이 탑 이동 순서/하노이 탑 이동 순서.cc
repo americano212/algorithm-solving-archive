e63e5b73f578f266ae3f79045ef0bdb987fa4a36
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-09-14 17:18:02 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 120 ms, Memory: 14436 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'000LL;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { int x; int y; int dir; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+vector<pii> ans;
+
+void hanoi(int n, int from, int to, int sub) {
+    if (n == 1) {
+        ans.push_back({ from,to });
+        return;
+    }
+    // n-1개를 보조로 이동
+    hanoi(n - 1, from, sub, to);
+    
+    // 제일 밑에 깔린거 이동
+    ans.push_back({ from,to });
+
+    hanoi(n - 1, sub, to, from);
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    hanoi(n, 1, 3, 2);
+
+    cout << ans.size() << endl;
+    FOR(i, 0, ans.size()) {
+        cout << ans[i].first << ' ' << ans[i].second << endl;
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
