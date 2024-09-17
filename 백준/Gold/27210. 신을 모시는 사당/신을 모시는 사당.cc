2eac2b1d3c06522d1941ef8d7a337a8abecb5035
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-09-17 22:29:26 +0900

    [Gold V] Title: 신을 모시는 사당, Time: 8 ms, Memory: 3588 KB -BaekjoonHub

---
+++ 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.cc
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'000LL;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { int x; int y; int dir; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> left_1(n);
+    vector<int> right_1(n);
+    int temp;
+    FOR(i, 0, n) {
+        cin >> temp;
+        if (temp == 1) {
+            left_1[i] = 1;
+            right_1[i] = -1;
+        }
+        else {
+            left_1[i] = -1;
+            right_1[i] = 1;
+        }
+    }
+    vector<int> dp_left(n + 1, 0);
+    vector<int> dp_right(n + 1, 0);
+    FOR(i, 1, n + 1) {
+        dp_left[i] = dp_left[i - 1] + left_1[i - 1];
+        dp_right[i] = dp_right[i - 1] + right_1[i - 1];
+    }
+
+    //FOR(i, 0, n + 1) {
+    //    cout << dp_left[i] << ' ';
+    //}
+    //cout << endl;
+    //FOR(i, 0, n + 1) {
+    //    cout << dp_right[i] << ' ';
+    //}
+    //cout << endl;
+    
+    ll minimum = 0;
+    ll ans = -INF;
+    
+    FOR(i, 1, n + 1) {
+        ans = max(ans, dp_left[i] - minimum);
+        if (dp_left[i] < minimum) {
+            minimum = dp_left[i];
+        }
+    }
+    minimum = 0;
+    FOR(i, 1, n + 1) {
+        ans = max(ans, dp_right[i] - minimum);
+        if (dp_right[i] < minimum) {
+            minimum = dp_right[i];
+        }
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
