bc1c5763b70b230cb38458606af8d7bd416f0719
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-09-18 23:36:09 +0900

    [Gold V] Title: 알약, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/4811. 알약/알약.cc
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'000LL;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { int x; int y; int dir; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+
+void solve() {
+    vector<ll> dp(31);
+    dp[1] = 1;
+    FOR(i, 2, 31) {
+        dp[i] = dp[i - 1] * (2 * i) * (2 * i - 1);
+        dp[i] = dp[i] / (i);
+        dp[i] = dp[i] / (i + 1);
+    }
+
+    while (true) {
+        int n;
+        cin >> n;
+        if (n == 0) break;
+
+        cout << dp[n] << endl;
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
