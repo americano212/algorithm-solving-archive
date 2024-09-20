247501c087b18b93b65ac7cb59d8878e9ddeab97
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-09-20 13:13:45 +0900

    [Gold IV] Title: 준표의 조약돌, Time: 8 ms, Memory: 4784 KB, Score: 250 point -BaekjoonHub

---
+++ 백준/Gold/15831. 준표의 조약돌/준표의 조약돌.cc
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'000LL;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { ll max_value; int max_index; ll now_value; int now_node; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+void solve() {
+    int n, b, w;
+    cin >> n >> b >> w;
+    string s;
+    cin >> s;
+    s = "0" + s;
+    vector<int> dp_b(n + 1, 0);
+    vector<int> dp_w(n + 1, 0);
+
+    FOR(i, 1, n + 1) {
+        dp_b[i] = dp_b[i - 1];
+        dp_w[i] = dp_w[i - 1];
+
+        if (s[i] == 'W') {
+            dp_w[i] += 1;
+        }
+        else {
+            dp_b[i] += 1;
+        }
+    }
+
+    int front = 0, back = 1;
+    int maximum = -INF;
+    while (back < n + 1) {
+        if (dp_b[back] - dp_b[front] <= b) {
+            if (dp_w[back] - dp_w[front] >= w) {
+                maximum = max(maximum, back - front);
+            }
+            back++;
+        }
+        else {
+            front++;
+        }
+    }
+    
+    if (maximum == -INF) {
+        cout << 0 << endl;
+    }
+    else {
+        cout << maximum << endl;
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
