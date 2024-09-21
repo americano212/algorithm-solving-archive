bbd4d3c15b0b8a93b5242ffebd88ad6dd134ed58
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-09-21 19:25:35 +0900

    [Gold V] Title: 김밥천국의 계단, Time: 16 ms, Memory: 6456 KB -BaekjoonHub

---
+++ 백준/Gold/28069. 김밥천국의 계단/김밥천국의 계단.cc
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'007LL;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { ll max_value; int max_index; ll now_value; int now_node; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+
+    vector<int> dp(n + 1, INF);
+    dp[0] = 0;
+   
+    queue<int> qu;
+    qu.push(0);
+
+    while (!qu.empty()) {
+        int now = qu.front();
+        qu.pop();
+
+        if (now + 1 < n + 1) {
+            if (dp[now + 1] > dp[now] + 1) {
+                dp[now + 1] = dp[now] + 1;
+                qu.push(now + 1);
+            }
+        }
+
+        if (now + now / 2 < n + 1) {
+            if (dp[now + now / 2] > dp[now] + 1) {
+                dp[now + now / 2] = dp[now] + 1;
+                qu.push(now + now / 2);
+            }
+        }
+    }
+    if (dp[n] <= k) {
+        cout << "minigimbob" << endl;
+    }
+    else {
+        cout << "water" << endl;
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
