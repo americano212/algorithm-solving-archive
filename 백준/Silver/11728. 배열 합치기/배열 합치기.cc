fd985fc7e8b4943311a3aedbf3aadb5bc56defcd
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-10-07 19:31:53 +0900

    [Silver V] Title: 배열 합치기, Time: 548 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.cc
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.cc
@@ -1,43 +1,53 @@
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <vector>
-#include <cmath>
-#include <functional>
-#include <climits>
-#include <stack>
-#include <queue>
-#include <deque>
-#include <map>
-#include <set>
-#include <cstring>
-#include <cassert>
-using ll = long long;
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'007LL;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
 using namespace std;
-ll inf = 1000000007;
 
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { ll max_value; int max_index; ll now_value; int now_node; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
 
-
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    vector<int> v(a + b);
+    FOR(i, 0, a + b) cin >> v[i];
+    sort(all(v));
+    FOR(i, 0, a + b) cout << v[i] << ' ';
+}
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
+    cin.tie(0)->sync_with_stdio(0);
 
-	int n, m;
-	cin >> n >> m;
-	vector<int> v(n + m);
-	for (int i = 0; i < n; i++) {
-		cin >> v[i];
-	}
-	for (int i = n; i < n + m; i++) {
-		cin >> v[i];
-	}
-	
-	sort(v.begin(), v.end());
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
 
-	for (int i = 0; i < n + m; i++) {
-		cout << v[i] << " ";
-	}
-
-	return 0;
+    return 0;
 }
