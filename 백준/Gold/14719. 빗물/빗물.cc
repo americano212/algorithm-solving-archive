b1d5a2ddf461fecc84a2b712181f793e0e882817
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-10-13 14:43:12 +0900

    [Gold V] Title: 빗물, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'007LL;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { ll max_value; int max_index; ll now_value; int now_node; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+
+void solve() {
+    int h, w;
+    cin >> h >> w;
+    vector<int> v(w);
+    FOR(i, 0, w) cin >> v[i];
+
+    vector<int> dp(w, 0);
+
+    FOR(i, 0, w) {
+        FOR(j, i + 1, w) {
+            if (v[i] <= v[j]) {
+                FOR(k, i + 1, j) {
+                    dp[k] = max(dp[k], v[i] - v[k]);
+                }
+            }
+        }
+    }
+    //FOR(i, 0, w) {
+    //    cout << dp[i] << ' ';
+    //}
+    //cout << endl;
+    for (int i = w - 1; i >= 0; i--) {
+        for (int j = i - 1; j >= 0; j--) {
+            if (v[i] <= v[j]) {
+                for (int k = i - 1; k > j; k--) {
+                    dp[k] = max(dp[k], v[i] - v[k]);
+                }
+            }
+        }
+    }
+    int ans = 0;
+    FOR(i, 0, w) {
+        ans += dp[i];
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
