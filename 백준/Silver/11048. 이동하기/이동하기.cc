fc3378c8a07bf191f3bc18fc25f78be6f89f0b24
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-10-17 19:13:01 +0900

    [Silver II] Title: 이동하기, Time: 68 ms, Memory: 9892 KB -BaekjoonHub

---
+++ 백준/Silver/11048. 이동하기/이동하기.cc
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1'000'000'007LL;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { ll max_value; int max_index; ll now_value; int now_node; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    matrix dp(n, vector<ll>(m));
+    FOR(i, 0, n) {
+        FOR(j, 0, m) {
+            cin >> dp[i][j];
+        }
+    }
+
+    int dx[3] = { -1,0,-1 };
+    int dy[3] = { 0,-1,-1 };
+
+    FOR(i, 0, n) {
+        FOR(j, 0, m) {
+            ll temp = 0;
+            FOR(idx, 0, 3) {
+                int bx = i + dx[idx];
+                int by = j + dy[idx];
+                if (btw(bx, 0, n) && btw(by, 0, m)) {
+                    temp = max(temp, dp[bx][by]);
+                }
+            }
+            dp[i][j] += temp;
+        }
+    }
+    cout << dp[n - 1][m - 1];
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
