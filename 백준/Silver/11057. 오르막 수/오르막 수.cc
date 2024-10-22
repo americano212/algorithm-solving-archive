117fbb9b8766786f2334c118494e27c06e28ff8a
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-10-22 19:50:05 +0900

    [Silver I] Title: 오르막 수, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.cc
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 10'007LL;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { ll max_value; int max_index; ll now_value; int now_node; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+
+void solve() {
+    int n;
+    cin >> n;
+    matrix dp(10, vector<ll>(n + 1, 0)); // 끝이 i고 길이가 j
+    FOR(i, 0, 10) {
+        dp[i][1] = 1;
+    }
+
+    FOR(j, 2, n + 1) {
+        FOR(i, 0, 10) {
+            FOR(bi, 0, i + 1) {
+                dp[i][j] += dp[bi][j - 1] % mod;
+                dp[i][j] %= mod;
+            }
+        }
+    }
+    ll ans = 0;
+    FOR(i, 0, 10) {
+        ans += dp[i][n] % mod;
+    }
+    cout << ans % mod << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
