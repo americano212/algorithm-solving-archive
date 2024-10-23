78b2c471e64d8da7d35e07c2d1e12db6db845209
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-10-24 00:24:29 +0900

    [Silver I] Title: 점프, Time: 0 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.cc
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 10'007LL;
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { ll max_value; int max_index; ll now_value; int now_node; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+
+void solve() {
+    int n;
+    cin >> n;
+    matrix mat(n, vector<ll>(n));
+    FOR(i, 0, n) {
+        FOR(j, 0, n) {
+            cin >> mat[i][j];
+        }
+    }
+    matrix dp(n, vector<ll>(n, 0));
+    dp[0][0] = 1;
+
+    FOR(i, 0, n) {
+        FOR(j, 0, n) {
+            if (i == n - 1 && j == n - 1) continue;
+            if (btw(i + mat[i][j], 0, n)) {
+                dp[i + mat[i][j]][j] += dp[i][j];
+            }
+            if (btw(j + mat[i][j], 0, n)) {
+                dp[i][j + mat[i][j]] += dp[i][j];
+            }
+        }
+    }
+    //FOR(i, 0, n) {
+    //    FOR(j, 0, n) {
+    //        cout << dp[i][j] << ' ';
+    //    }
+    //    cout << endl;
+    //}
+
+    cout << dp[n - 1][n - 1] << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
