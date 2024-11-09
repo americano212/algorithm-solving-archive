abd057d8d66af94f961ffb274ec48beee8177e44
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2024-11-09 23:53:39 +0900

    [Silver V] Title: 이름궁합 테스트, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/17269. 이름궁합 테스트/이름궁합 테스트.cc
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 9901LL;
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { ll max_value; int max_index; ll now_value; int now_node; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+void solve() {
+    int check[26] = { 3,2,1,2,4,3,1,3,1,1,3,1,3,2,1,2,2,2,1,2,1,1,1,2,2,1 };
+    int n, m;
+    cin >> n >> m;
+    vector<int> v;
+    string s1, s2;
+    cin >> s1 >> s2;
+    int a = 0, b = 0;
+    while (true) {
+        if (a == n && b == m) break;
+
+        if (a == n) {
+            v.push_back(check[s2[b] - 'A']);
+            b++;
+            continue;
+        }
+        if (b == m) {
+            v.push_back(check[s1[a] - 'A']);
+            a++;
+            continue;
+        }
+
+        if (a == b) {
+            v.push_back(check[s1[a] - 'A']);
+            a++;
+            continue;
+        }
+        else {
+            v.push_back(check[s2[b] - 'A']);
+            b++;
+            continue;
+        }
+    }
+    //FOR(i, 0, v.size()) {
+    //    cout << v[i] << ' ';
+    //}
+    int cnt = n + m;
+    FOR(i, 0, cnt - 2) {
+        FOR(j, 1, cnt - i) {
+            v[j - 1] = (v[j - 1] + v[j]) % 10;
+        }
+    }
+    //cout << endl;
+    //FOR(i, 0, v.size()) {
+    //    cout << v[i] << ' ';
+    //}
+    if (v[0] != 0) cout << v[0];
+    cout << v[1] << '%' << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
