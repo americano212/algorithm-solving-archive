8683729a1921868e1582fcfbc7d87852c5d3c13d
americano212 <wq0212@naver.com>
2024-12-18 18:59:06 +0900

    feat: erase unused code

--- solved.py
+++
@@ -1,8 +0,0 @@
-score_save = []
-for i in range(1, 21):
-    problem = 553 + i
-    score = 175 * (1 - 0.995**problem)
-    score_before = 175 * (1 - 0.995**(problem - 1))
-    if (int(score) != int(score_before)):
-        print('*'*50)
-    print("{}문제 후".format(i), score)
