6ef8d47b97f960aed6619f25d4cf74ebdcf098c7
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2025-03-13 22:42:06 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.cc
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1234567891LL;
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { ll max_value; int max_index; ll now_value; int now_node; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<string> mat(n);
+    FOR(i, 0, n) {
+        cin >> mat[i];
+    }
+
+    int ans = 64;
+    FOR(start_x, 0, n-7) {
+        FOR(start_y, 0, m-7) {
+            int w = 0;
+            int b = 0;
+            FOR(i, start_x, start_x + 8) {
+                FOR(j, start_y, start_y + 8) {
+                    if ((i + j) % 2 == 0) {
+                        if (mat[i][j] == 'B') w++;
+                        if (mat[i][j] == 'W') b++;
+                    }
+                    else {
+                        if (mat[i][j] == 'W') w++;
+                        if (mat[i][j] == 'B') b++;
+                    }
+                }
+            }
+            ans = min(ans, min(w, b));
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
