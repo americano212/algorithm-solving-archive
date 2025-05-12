8d3049b815680c2d3b3f79010c212b7bf9cb0258
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2025-05-12 17:24:36 +0900

    [Bronze IV] Title: $A + B - C$, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.cc
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+#define FOR(x, y, n) for (int x = y; x < (n); x++)
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a *b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x, a, b) a <= x &&x < b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 10007LL;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, -1, 0, 1};
+int dx6[6] = {1, -1, 0, 0, 0, 0};
+int dy6[6] = {0, 0, 1, -1, 0, 0};
+int dz6[6] = {0, 0, 0, 0, 1, -1};
+int dx8[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+int dy8[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+using namespace std;
+
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b - c << endl;
+    if (b < 10) {
+        cout << a * 10 + b - c << endl;
+    } else if (b < 100) {
+        cout << a * 100 + b - c << endl;
+    } else if (b < 1000) {
+        cout << a * 1000 + b - c << endl;
+    } else {
+        cout << a * 10000 + b - c << endl;
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)
+        solve();
+
+    return 0;
+}
