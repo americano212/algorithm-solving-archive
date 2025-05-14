433fb5a8e6faba496b019c381055e273b869b05c
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2025-05-14 09:14:48 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define FOR(x, y, n) for (int x = y; x < (n); x++)
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a *b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x, a, b) a <= x &&x < b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 10007LL;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, -1, 0, 1};
+int dx6[6] = {1, -1, 0, 0, 0, 0};
+int dy6[6] = {0, 0, 1, -1, 0, 0};
+int dz6[6] = {0, 0, 0, 0, 1, -1};
+int dx8[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+int dy8[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+using namespace std;
+
+
+void solve() {
+    priority_queue<int> pq;
+    int t;
+    cin >> t;
+    FOR(_, 0, t) {
+        int n;
+        cin >> n;
+        if (n == 0) {
+            if (pq.empty()) {
+                cout << 0 << endl;
+            } else {
+                cout << pq.top() << endl;
+                pq.pop();
+            }
+        } else {
+            pq.push(n);
+        }
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)
+        solve();
+
+    return 0;
+}
