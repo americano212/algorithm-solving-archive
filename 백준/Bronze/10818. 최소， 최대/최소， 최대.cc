82a65b24e077f83f26740b67fce7d0c0c7d84af2
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2025-05-15 21:02:46 +0900

    [Bronze III] Title: 최소, 최대, Time: 96 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.cc
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n'
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 1234567891LL;
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { ll max_value; int max_index; ll now_value; int now_node; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+struct compare {
+    bool operator()(const pii& a, const pii& b) const {
+        if (a.first > b.first) return true;
+        if (a.first < b.first) return false;
+        return a.second > b.second;
+    }
+};
+
+void solve() {
+    int n;
+    cin >> n;
+    ll m;
+    ll _min = INF, _max = -INF;
+    FOR(i, 0, n) {
+        cin >> m;
+        _min = min(_min, m);
+        _max = max(_max, m);
+    }
+
+    cout << _min << ' ' << _max << endl;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
