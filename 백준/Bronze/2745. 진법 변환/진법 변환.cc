e3b7c97d42e4445040470435674f19673be7d64c
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2025-05-19 12:45:38 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.cc
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define FOR(x, y, n) for (int x = y; x < (n); x++)
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a *b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x, a, b) a <= x &&x < b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 10007LL;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, -1, 0, 1};
+int dx6[6] = {1, -1, 0, 0, 0, 0};
+int dy6[6] = {0, 0, 1, -1, 0, 0};
+int dz6[6] = {0, 0, 0, 0, 1, -1};
+int dx8[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+int dy8[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+using namespace std;
+
+
+void solve() {
+    string s;
+    cin >> s;
+    ll n;
+    cin >> n;
+    ll ans = 0;
+    for (ll i = s.length() - 1; i >= 0; i--) {
+        if (s[i] >= '0' && s[i] <= '9') {
+            ll temp = (ll) (s[i] - '0');
+            FOR(j, 0, (s.length() - 1) - i) { temp *= n; }
+            ans += temp;
+        } else {
+            ll temp = (ll) (s[i] - 'A' + 10);
+            FOR(j, 0, (s.length() - 1) - i) { temp *= n; }
+            ans += temp;
+        }
+    }
+    cout << ans << endl;
+}
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)
+        solve();
+
+    return 0;
+}
