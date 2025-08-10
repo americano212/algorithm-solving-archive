05b6f51f914581fd6b5f93cc9e269b44fee45d1d
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2025-08-10 19:06:36 +0900

    [Silver III] Title: N과 M (3), Time: 344 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#define FOR(x, y, n) for (int x = y; x < (n); x++)
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a *b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x, a, b) a <= x &&x < b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 10007LL;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, -1, 0, 1};
+int dx6[6] = {1, -1, 0, 0, 0, 0};
+int dy6[6] = {0, 0, 1, -1, 0, 0};
+int dz6[6] = {0, 0, 0, 0, 1, -1};
+int dx8[8] = {0, -1, -1, -1, 0, 1, 1, 1};
+int dy8[8] = {1, 1, 0, -1, -1, -1, 0, 1};
+
+using namespace std;
+
+matrix mat(31, vector<ll>(31, -1));
+int n, m;
+
+vector<int> ans(8);
+void dfs(int x, int depth) {
+    ans[depth] = x;
+    if (depth == m) {
+        FOR(i, 1, m + 1) { cout << ans[i] << ' '; }
+        cout << endl;
+        return;
+    }
+
+    FOR(next, 1, n + 1) { dfs(next, depth + 1); }
+}
+
+void solve() {
+    cin >> n >> m;
+
+    dfs(0, 0);
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)
+        solve();
+
+    return 0;
+}
