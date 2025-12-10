b93d21f612fa9fa6a75f13f4ab61d952792a2eb6
Dongjun Shin <53344349+americano212@users.noreply.github.com>
2025-12-11 01:19:54 +0900

    [Silver V] Title: 카약, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/2890. 카약/카약.cc
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+#define FOR(x,y,n)  for(int x=y;x<(n);x++) 
+#define endl '\n';
+#define INF 2'147'483'647LL
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pllp pair<ll, pll>
+#define pff pair<float, float>
+#define pdd pair<double, double>
+#define pldd pair<ld, ld>
+#define matrix vector<vector<ll>>
+#define lcm(a, b) a * b / gcd(a, b)
+#define all(v) v.begin(), v.end()
+#define btw(x,a,b) a<=x && x<b
+ll gcd(ll a, ll b) { return (b ? gcd(b, a % b) : a); }
+
+const ll mod = 10007LL;
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,-1,0,1 };
+int dx6[6] = { 1,-1,0,0,0,0 };
+int dy6[6] = { 0,0,1,-1,0,0 };
+int dz6[6] = { 0,0,0,0,1,-1 };
+int dx8[8] = { 0,-1,-1,-1,0,1,1,1 };
+int dy8[8] = { 1,1,0,-1,-1,-1,0,1 };
+
+using namespace std;
+
+struct EDGE { ll start; ll end; ll cost; };
+struct DOT3 { ll x; ll y; ll z; };
+struct NODE { ll max_value; int max_index; ll now_value; int now_node; };
+struct ITEM { bool isNum; int num = 0; char bracket = ' '; };
+struct MOVE { int x; int y; int time; };
+
+
+
+void solve() {
+    int r, c;
+    cin >> r >> c;
+    vector<string> mat(r);
+    for (int i = 0; i < r; i++) {
+        cin >> mat[i];
+    }
+
+    vector<int> rates(10, -1);
+
+    int rate = 1;
+    while (c > 0) {
+        c--;
+        bool flag = false;
+        for (int i = 0; i < r; i++) {
+            if ('1' <= mat[i][c] && mat[i][c] <= '9') {
+                int now = (int)(mat[i][c] - '0');
+                if (rates[now] == -1) {
+                    rates[now] = rate;
+                    flag = true;
+                }
+            }
+        }
+        if (flag) rate++;
+    }
+
+    for (int i = 1; i < 10; i++) {
+        cout << rates[i] << endl;
+    }
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)solve();
+
+    return 0;
+}
